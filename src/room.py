6b365c29e8218cc3a507775d6715489eafa71e8c
Rob Salzberg <robsalzberg@gmail.com>
2019-05-16 21:46:27 -0400

    Refactored room class and added items

--- src/room.py
+++ src/room.py
@@ -6,9 +6,4 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-
-    def __repr__(self):
-        output = ""
-        output += "Room: " + self.name + "\n"
-        output += "Description: " + self.description
-        return output
+        self.items = []
